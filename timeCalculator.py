454df21b1892c5e4ec3369b4fc64f03c5bd6bea5
Amanda Kuenzle <amanda.kuenzle@buhlergroup.com>
2019-07-22 16:15:45 +0200

    extendet the functionality

--- timeCalculator.py
+++ timeCalculator.py
@@ -1,5 +1,36 @@
 #Work time calculator
 
-startTime = int(input("enter your start time in format 0813:   "))
+import math
 
-print("your start time is", startTime)
+breakTime = 0.25
+
+#start time input and calculation
+startTime = input("enter your start time in format 7:00 for example   ")
+
+startTimeList = startTime.split(':')
+
+calculatedStartTime = float(startTimeList[0]) + float(startTimeList[1]) / 60
+
+print("your start time is", calculatedStartTime)
+
+#work time input and calculation
+workingTime = input("how many hours do you want to work? enter in format 8:15 for example   ")
+
+workingTimeList = workingTime.split(':')
+
+calculateWorkTime = float(workingTimeList[0]) + float(workingTimeList[1]) / 60
+
+print("your target working time is (with break)  ", calculateWorkTime)
+
+
+calculatedEndTimeDecimal = calculatedStartTime + calculateWorkTime
+
+print("you can finish work at", calculatedEndTimeDecimal)
+
+
+calculatedEndTime = math.modf(calculatedEndTimeDecimal)
+
+print(calculatedEndTime)
+
+
+
