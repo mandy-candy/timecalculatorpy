2935bee9c3f1bc48897d8f580f5ef53bf688d0a5
Amanda Kuenzle <amanda.kuenzle@buhlergroup.com>
2019-07-23 16:34:14 +0200

    Added time conversion

--- timeCalculator.py
+++ timeCalculator.py
@@ -6,15 +6,17 @@ breakTime = 0.25
 
 #start time input and calculation
 startTime = input("enter your start time in format 7:00 for example   ")
+#startTime = "07:22"
 
 startTimeList = startTime.split(':')
 
 calculatedStartTime = float(startTimeList[0]) + float(startTimeList[1]) / 60
 
-print("your start time is", calculatedStartTime)
+print("your start time is", round(calculatedStartTime, 2))
 
 #work time input and calculation
 workingTime = input("how many hours do you want to work? enter in format 8:15 for example   ")
+#workingTime = "08:30"
 
 workingTimeList = workingTime.split(':')
 
@@ -25,12 +27,21 @@ print("your target working time is (with break)  ", calculateWorkTime)
 
 calculatedEndTimeDecimal = calculatedStartTime + calculateWorkTime
 
-print("you can finish work at", calculatedEndTimeDecimal)
+print("you can finish work at", round(calculatedEndTimeDecimal, 2))
 
 
 calculatedEndTime = math.modf(calculatedEndTimeDecimal)
 
-print(calculatedEndTime)
+calculatedEndHours = calculatedEndTime[1]
+calculatedEndMinutes = calculatedEndTime[0] * 60 / 100
+
+#print(calculatedEndHours)
+#print(round(calculatedEndMinutes, 2))
+
+EndTime = calculatedEndHours + round(calculatedEndMinutes, 2)
+
+print(EndTime)
+
 
 
 
