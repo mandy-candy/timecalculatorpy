49f1eb98fa85e864e2c916b9e93b1d954da08466
Amanda Kuenzle <amanda.kuenzle@buhlergroup.com>
2019-07-24 10:07:56 +0200

    changed calculation, seems to work now

--- timeCalculator.py
+++ timeCalculator.py
@@ -1,35 +1,42 @@
 #Work time calculator
 
+#Amanda Künzle, 22.07.2019
+
+#--------------------------------------------------#
+
+#https://github.com/mandy-candy/timecalculatorpy.git
+
+#--------------------------------------------------#
+
+
 import math
 
 breakTime = 0.25
 lunchBreak = 0.25
 
 #start time input and calculation
-startTime = input("enter your start time in format 7:00 for example   ")
+startTime = input("Enter your start time in format 7:00 for example   ")
 #startTime = "07:22"
 
 startTimeList = startTime.split(':')
 
 calculatedStartTime = float(startTimeList[0]) + float(startTimeList[1]) / 60
 
-print("your start time is", round(calculatedStartTime, 2))
+#print("your start time is   ", round(calculatedStartTime, 2))
 
 #work time input and calculation
-workingTime = input("how many hours do you want to work? enter in format 8:15 for example   ")
-#workingTime = "08:30"
+workingTime = input("How many hours do you want to work? enter in format 8:15 for example   ")
+#workingTime = "06:30"
 
 workingTimeList = workingTime.split(':')
 
 calculateWorkTime = breakTime + lunchBreak + float(workingTimeList[0]) + float(workingTimeList[1]) / 60
 
-print("your target working time is (with break)  ", calculateWorkTime)
-
+print("Your target working time is (with break):   ", round(calculateWorkTime, 2))
 
 calculatedEndTimeDecimal = calculatedStartTime + calculateWorkTime
 
-print("you can finish work at", round(calculatedEndTimeDecimal, 2))
-
+#print("you can finish work at   ", round(calculatedEndTimeDecimal, 2))
 
 calculatedEndTime = math.modf(calculatedEndTimeDecimal)
 
@@ -37,11 +44,11 @@ calculatedEndHours = calculatedEndTime[1]
 calculatedEndMinutes = calculatedEndTime[0] * 60 / 100
 
 #print(calculatedEndHours)
-print(round(calculatedEndMinutes, 2))
+#print(round(calculatedEndMinutes, 2))
 
 EndTime = calculatedEndHours + round(calculatedEndMinutes, 2)
 
-print(EndTime)
+print("You can finish work at:   ", EndTime)
 
 
 
